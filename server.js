6d7b8f639928da049cb7bc05b7914ad14ad1b1e8
asBrettisay <brett.caudill.is@gmail.com>
2016-03-04 15:03:39 -0700

    Added a debugger logger.

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var express = require('express'),
 
 var app = express();
 
+var logger = function(req, res, next) {
+  console.log(req.body);
+  next();
+}
+
 app.use(middleware.addHeaders)
 
 app.use(bodyParser.json());
@@ -24,7 +29,7 @@ app.put('/location', mainCtrl.putLocation);
 
 app.post('/hobbies', mainCtrl.postHobbies);
 app.post('/occupations', mainCtrl.postOccupations);
-app.post('/skillz', middleware.generateId, mainCtrl.postSkillz);
+app.post('/skillz', logger, middleware.generateId, mainCtrl.postSkillz);
 
 app.listen(8001, function() {
   console.log('Listening on 8001');
